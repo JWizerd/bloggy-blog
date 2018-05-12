7c230137c991dd561c53cd398da398fd6e1bbcbf
JWizerd <jwodke@outlook.com>
2018-05-12 16:50:40 -0600

    disqus added

--- assets/vendor/startbootstrap-clean-blog/js/jqBootstrapValidation.js
+++ assets/vendor/startbootstrap-clean-blog/js/jqBootstrapValidation.js
@@ -17,7 +17,7 @@
       prependExistingHelpBlock: false,
       sniffHtml: true, // sniff for 'required', 'maxlength', etc
       preventSubmit: true, // stop the form submit event from firing if validation fails
-      submitError: true, // function called if there is an error when trying to submit
+      submitError: false, // function called if there is an error when trying to submit
       submitSuccess: false, // function called just before a successful submit event is sent to the server
       semanticallyStrict: false, // set to true to tidy up generated HTML output
       autoAdd: {
